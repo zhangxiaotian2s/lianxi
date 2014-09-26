53a2c7da453900e1986cec7327bef1bb16da905a
zhangxiaotiangit <712023231@qq.com>
2014-09-26 11:19:10 +0800

    926
    926

---
+++ 上下循环滚动尝试/updownMove.js
@@ -0,0 +1,75 @@
+(function($) {
+	$.fn.updownMove=function(options,scrollid){
+		var scrollmain =$(scrollid)
+		Defaults = {
+			speed:1000,
+			isauto:0,
+			autotimer:2000,
+			direction :'down',
+			boxchild:'li',
+			upbtn:'.upbtn',
+			downbtn:'.downbtn' 
+			}	
+		options=$.extend({},Defaults,options)
+		
+	var timer;
+	//自上而下
+	function downscrollfn(){
+		scrollmain.children(options.boxchild).eq(scrollmain.children(options.boxchild).length-1).hide(1000,function(){
+			$(this).prependTo(scrollid).slideDown(options.speed)
+			
+			})
+		}
+	//自下而上	
+	function upscrollfn(){
+		  scrollmain.children(options.boxchild).eq(0).slideUp(options.speed,function(){
+          $(this).appendTo(scrollid).show()  
+		})	
+	}
+	//自动的处理
+	function autoactionfn(){
+		if(options.isauto){
+		if(options.direction=='up'){
+		timer= setInterval(upscrollfn,options.autotimer)		
+		}
+		else if(options.direction=='down'){
+		 timer= setInterval(downscrollfn,options.autotimer)	
+		}	
+		}
+	}
+	//向下按纽处理
+	$(options.downbtn).bind('click ',function(){
+		downscrollfn()
+	 })
+	$(".scrollbox").touchwipe({
+		wipeLeft:function(){
+		  downscrollfn()
+			},
+		wipeRight:function(){
+			alert("cccc")
+		 }
+		
+		})
+	 
+	//向上按纽处理
+	$(options.upbtn).bind('click',function(){
+		upscrollfn()
+	})
+	//暂停的处理
+	function hoverstopfn(element){
+		$(element).hover(function(){
+		clearInterval(timer)	
+		},function(){
+		autoactionfn()
+	   })	
+	  }
+	 hoverstopfn(options.upbtn)
+	 hoverstopfn(options.downbtn)
+	 hoverstopfn(scrollid)
+	//自动生效的处理
+	
+		autoactionfn()
+
+	
+	}
+})(jQuery)
